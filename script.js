8286952c186946cc655b62a86954d857d497b869
Thiago Oliveira <thiagomodev@gmail.com>
2022-10-13 17:34:10 -0300

    add get winner function

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 
 const rockPaperScissors = ['Rock', 'Paper', 'Scissors'];
-let playerChoice = Number(prompt("Make your choice, 0 for Rock, 1 for Paper and 2 for Scissors"));
+let playerChoice = prompt("Make your choice, 0 for Rock, 1 for Paper and 2 for Scissors");
 
-const computerChoice = computer => {
-    return computer = rockPaperScissors[Math.floor(Math.random() * (3 - 0) + 0)];
+const computerChoice = () => {
+    return rockPaperScissors[Math.floor(Math.random() * (3 - 0) + 0)];
 }
 
-const player = player => {
-    return player = rockPaperScissors[playerChoice];
+const player = () => {
+    return rockPaperScissors[playerChoice];
 }
+
+const getWinner = (computer, player) => {
+    if (player == computer) {
+        return console.log('Draw');
+    }
+    if (player == 'Rock') {
+        if (computer == 'Paper') return console.log(`You lost!! You chose ${player} and computer chose ${computer}`);
+        if (computer == 'Scissors') return console.log(`You won!! You chose ${player} and computer chose ${computer}`);
+    }
+    if (player == 'Paper') {
+        if (computer == 'Rock') return console.log(`You won!! You chose ${player} and computer chose ${computer}`);
+        if (computer == 'Scissors') return console.log(`You lost!! You chose ${player} and computer chose ${computer}`);
+    }
+    if (player == 'Scissors') {
+        if (computer == 'Rock') return console.log(`You lost!! You chose ${player} and computer chose ${computer}`);
+        if (computer == 'Paper') return console.log(`You won!! You chose ${player} and computer chose ${computer}`);
+    }
+}
+getWinner(computerChoice(), player());
 
 
