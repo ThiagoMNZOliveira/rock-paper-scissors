86596a7e5ebd76cffcd57556ffbc5c626a22b92b
Thiago Oliveira <thiagomodev@gmail.com>
2022-10-13 16:56:09 -0300

    add player choice

--- script.js
+++ script.js
@@ -1,9 +1,13 @@
 
 const rockPaperScissors = ['Rock', 'Paper', 'Scissors'];
-let computer;
+let playerChoice = Number(prompt("Make your choice, 0 for Rock, 1 for Paper and 2 for Scissors"));
 
-function getComputerChoice(computer) {
+const computerChoice = computer => {
     return computer = rockPaperScissors[Math.floor(Math.random() * (3 - 0) + 0)];
 }
 
-getComputerChoice(computer);
+const player = player => {
+    return player = rockPaperScissors[playerChoice];
+}
+
+
