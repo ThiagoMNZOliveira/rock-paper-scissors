74a15ffa656e9604616e7ff71c09c45ebdf94149
Thiago Oliveira <thiagomodev@gmail.com>
2022-10-24 16:26:22 -0300

    add ui

--- script.js
+++ script.js
@@ -1,62 +1,75 @@
 let score = [0, 0];
 
-function game() {
+const buttons = document.querySelector('.buttons');
+
+
+buttons.addEventListener('click', e => {
+    const button = e.target;
+    let playerChoice;
+    if(button.classList.contains('rock')) {
+        playerChoice = 'Rock';
+        game(playerChoice);
+    }
+    if(button.classList.contains('paper')) {
+        playerChoice = 'Paper';
+        game(playerChoice);
+    }
+    if(button.classList.contains('scissors')) {
+        playerChoice = 'Scissors';
+        game(playerChoice);
+    }
+})
+
+function game(player) {
     const rockPaperScissors = ['Rock', 'Paper', 'Scissors'];
+    const display = document.querySelector('.display');
 
     const computerChoice = () => {
         return rockPaperScissors[Math.floor(Math.random() * (3 - 0) + 0)];
     }
 
-    const player = () => {
-        let playerChoice = prompt("Make your choice, 0 for Rock, 1 for Paper and 2 for Scissors");
-        return rockPaperScissors[playerChoice];
-    }
-
     const getWinner = (computer, player) => {
         if (player == computer) {
-            return console.log('Draw');
+            return display.innerText = `${player} x ${computer}`;
         }
         if (player == 'Rock') {
             if (computer == 'Paper') {
                 score[1]++;
-                return console.log(`You lose!! You chose ${player} and computer chose ${computer}`);
+                return display.innerText = `You lose!! You chose ${player} and computer chose ${computer}`;
             }
             if (computer == 'Scissors') {
                 score[0]++;
-                return console.log(`You won!! You chose ${player} and computer chose ${computer}`);
+                return display.innerText = `You won!! You chose ${player} and computer chose ${computer}`;
             }
         }
         if (player == 'Paper') {
             if (computer == 'Rock') {
                 score[0]++;
-                return console.log(`You won!! You chose ${player} and computer chose ${computer}`);
+                return display.innerText = `You won!! You chose ${player} and computer chose ${computer}`;
             }
             if (computer == 'Scissors') {
                 score[1]++;
-                return console.log(`You lose!! You chose ${player} and computer chose ${computer}`);
+                return display.innerText = `You lose!! You chose ${player} and computer chose ${computer}`;
             }
         }
         if (player == 'Scissors') {
             if (computer == 'Rock') {
                 score[1]++;
-                return console.log(`You lose!! You chose ${player} and computer chose ${computer}`);
+                return display.innerText = `You lose!! You chose ${player} and computer chose ${computer}`;
             }
             if (computer == 'Paper') {
                 score[0]++;
-                return console.log(`You won!! You chose ${player} and computer chose ${computer}`);
+                return display.innerText = `You won!! You chose ${player} and computer chose ${computer}`;
             }
         }
     }
-    getWinner(computerChoice(), player());
-
+    getWinner(computerChoice(), player);
+    if(score[0] >= 5 || score[1] >= 5) {
+        display.innerText = `Player || Computer
+        ${score[0]}  ${score[1]}`
+    }
 }
 
-for (let i = 0; i < 5; i++) {
-    game();
-}
-
-console.log(`Player || Computer
-     ${score[0]} || ${score[1]}`);
 
 
 
